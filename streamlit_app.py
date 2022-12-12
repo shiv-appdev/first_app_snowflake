96f41d887a4bdce1f3be1e30769fb543fccdb844
shiv-appdev <120260601+shiv-appdev@users.noreply.github.com>
2022-12-13 01:20:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ streamlit.text('apple pie')
 streamlit.text('Build your own fruit smoothie')
 
 import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt", engine='pyhton', usecols=["Fruit", "Serving_Gram_Weight", "Total_Fat_G"])
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 # Display the table on the page.
 streamlit.dataframe('my_fruit_list')
