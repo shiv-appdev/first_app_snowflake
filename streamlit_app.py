d982d50cb95a00399ece16678b719d53321ea637
shiv-appdev <120260601+shiv-appdev@users.noreply.github.com>
2022-12-13 00:37:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas
 
 streamlit.title('JK bakers')
 streamlit.header('FRUITS AND JUICES ')
-streamlit.text('banans')
+
 streamlit.text('smoothie ')
 streamlit.text('apple pie')
 streamlit.text('Build your own fruit smoothie')
