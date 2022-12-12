00fbd8f3a35e3e9ff1acd32870a927616307035e
shiv-appdev <120260601+shiv-appdev@users.noreply.github.com>
2022-12-12 23:51:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ streamlit.title('JK bakers')
 
 streamlit.header('FRUITS AND JUICES ')
 streamlit.text('banans')
-streamlit.text('smoothie :P')
-streamlit.text("apple pie')
+streamlit.text('smoothie ')
+streamlit.text('apple pie')
 streamlit.text('contact details -7889491152')
 
