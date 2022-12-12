2f260de20dd98633362583728d0ae97634bd3f0b
shiv-appdev <120260601+shiv-appdev@users.noreply.github.com>
2022-12-13 00:36:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,6 @@ streamlit.text('apple pie')
 streamlit.text('Build your own fruit smoothie')
 
 
-
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
