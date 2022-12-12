5e03bbedd3cf9a9188b52c8888d1743c262ecf82
shiv-appdev <120260601+shiv-appdev@users.noreply.github.com>
2022-12-13 01:21:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 # Display the table on the page.
-streamlit.dataframe('my_fruit_list')
+streamlit.dataframe(my_fruit_list)
 
 
 
