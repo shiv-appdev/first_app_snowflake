05c7317078ecd37ce16a6eb7cc25b381c86d04f4
shiv-appdev <120260601+shiv-appdev@users.noreply.github.com>
2022-12-12 23:34:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ streamlit.header('Menu')
 streamlit.text('VegPuff')
 streamlit.text('chickenPuff')
 streamlit.text('vegsandwich')
-streamlit.footer('best puffs and sandwiches')
+streamlit.text('best puffs and sandwiches')
 
