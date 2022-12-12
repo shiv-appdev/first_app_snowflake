5bce39f5d4cf05074e7cbe5b6b1f1c797584bdc0
shiv-appdev <120260601+shiv-appdev@users.noreply.github.com>
2022-12-13 01:03:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,9 @@ streamlit.text('smoothie ')
 streamlit.text('apple pie')
 streamlit.text('Build your own fruit smoothie')
 
-import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+import pandas as pd
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
 streamlit.dataframe('my_fruit_list')
 
 #Let us put a pick list here so they can pick the fruit they want to include 
