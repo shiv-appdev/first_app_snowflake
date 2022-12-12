2fa445984ac90feba803df716a2a47df79e5afbe
shiv-appdev <120260601+shiv-appdev@users.noreply.github.com>
2022-12-13 01:25:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,6 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 streamlit.dataframe(my_fruit_list)
 # Display the table on the page.
 
-my_fruit_list = my_fruit_list.set_index(Fruit)
+
 
 
